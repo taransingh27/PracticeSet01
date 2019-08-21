84fe1eadebc7c8a12c84b1c5a7c8bd9cd45d1f41
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-08-21 15:47:14 +0530

    print output and save the program:Question1.java

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -5,11 +5,8 @@ public class Question1 {
     public static void main(String[] args) {
         Scanner Scn = new Scanner(System.in);
         String userName = input.nextline().trim();
-
-
+        System.out.println("Welcome, " + userName + "!");
         Scn.close();
-
-
     }
 
 }
