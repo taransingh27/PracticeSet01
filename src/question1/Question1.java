70148c22f5dedd4182327c17e3c740c5c246f351
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-08-21 15:42:11 +0530

    create object for scanner class:Question1.java

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -1,8 +1,12 @@
 package question1;
 
-
+import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
+        Scanner Scn = new Scanner(System.in);
+
+        Scn.close();
+
 
     }
 
