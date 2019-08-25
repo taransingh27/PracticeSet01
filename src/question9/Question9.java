42b5d6d8f0c9c232f0f568a8b7f14d0af24af9ab
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-08-25 20:08:51 +0530

    created logic for question9 to convert lowercase to upper and upper to lower:Question9.java

--- src/question9/Question9.java
+++ src/question9/Question9.java
@@ -1,4 +1,15 @@
 package question9;
 
+import java.util.Scanner;
+
 public class Question9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+        if (Character.isLowerCase(a)) {
+            System.out.println(Character.toUpperCase(a));
+        } else {
+            System.out.println(Character.toLowerCase(a));
+        }
+    }
 }
