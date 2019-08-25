92cb41324a98d4641abf91749a8746616760912b
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-08-25 19:18:51 +0530

    created java class for question4 and logic to check no. is +ve and -ve :Question4.java

---
+++ src/question4/Question4.java
@@ -0,0 +1,19 @@
+package question4;
+
+import java.util.Scanner;
+
+public class Question4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a < 0) {
+            System.out.println("negative");
+        } else if (a > 0) {
+            System.out.println("positive");
+        } else {
+            System.out.println("zero");
+        }
+
+
+    }
+}
