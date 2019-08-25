a950f4963e329fcd5686966df78bdb13801589df
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-08-25 22:30:34 +0530

    creating logic to check number is palindrome or not question14:Question14.java

--- src/question14/Question14.java
+++ src/question14/Question14.java
@@ -1,4 +1,20 @@
 package question14;
 
+import java.util.Scanner;
+
 public class Question14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt(), reversedInteger = 0, remainder, originalInteger;
+        originalInteger = num;
+        while (num != 0) {
+            remainder = num % 10;
+            reversedInteger = reversedInteger * 10 + remainder;
+            num /= 10;
+        }
+        if (originalInteger == reversedInteger)
+            System.out.println(originalInteger + " is a palindrome.");
+        else
+            System.out.println(originalInteger + " is not a palindrome.");
+    }
 }
