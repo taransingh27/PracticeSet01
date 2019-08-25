bd5f4d7cb3db8881211ffa9bde39a7fbc8f3c6a5
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-08-25 22:03:24 +0530

    creating logic to find sum of digits of a number for question13:Question13.java

--- src/question13/Question13.java
+++ src/question13/Question13.java
@@ -1,4 +1,16 @@
 package question13;
 
+import java.util.Scanner;
+
 public class Question13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int sum = 0;
+        while (a != 0) {
+            sum = sum + a % 10;
+            a = a / 10;
+        }
+        System.out.println(sum);
+    }
 }
