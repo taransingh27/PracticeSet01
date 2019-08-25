bed6547a2e355164a44df2c5bbd7896253bc219f
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-08-25 19:49:53 +0530

    created logic for question8 :Question8.java

--- src/question8/Question8.java
+++ src/question8/Question8.java
@@ -1,4 +1,25 @@
 package question8;
 
+import java.util.Scanner;
+
 public class Question8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+        int b = sc.nextInt();
+        if (a == 'f') {
+            if (b >= 1 && b <= 58) {
+                System.out.println("8.2%");
+            } else {
+                System.out.println("9.2%");
+            }
+        } else {
+            if (b >= 1 && b <= 58) {
+                System.out.println("8.4%");
+            } else {
+                System.out.println("10.5%");
+            }
+        }
+    }
+
 }
