93c2d3c19db3135ba455526c7802bdbc4fa65078
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-08-25 18:55:49 +0530

    build logic for question 2 :Question2.java

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
         Scanner Scn = new Scanner(System.in);
-        String userName = input.nextline().trim();
+        String userName = Scn.nextLine().trim();
         System.out.println("Welcome, " + userName + "!");
         Scn.close();
     }
