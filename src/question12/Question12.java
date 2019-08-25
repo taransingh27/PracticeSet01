7cf018771544e12f9982222efc81da11dd67c1cb
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-08-25 21:54:35 +0530

    creating logic for question12 to check prime and non prime number:Question12.java

--- src/question12/Question12.java
+++ src/question12/Question12.java
@@ -1,5 +1,21 @@
 package question12;
 
-public class Question12 {
+import java.util.Scanner;
 
+public class Question12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        boolean flag = false;
+        for (int i = 2; i <= num / 2; ++i) {
+            if (num % i == 0) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
+    }
 }
